4b48938a4e30a41cfb88a5563a5317be997d713c
dbershadsky <dbershadsky@appdynamics.com>
2014-02-28 11:13:15 -0800

    adding monitoring option

--- src/main/custom/actions/opsGenie/opsGenie.sh
+++ src/main/custom/actions/opsGenie/opsGenie.sh
@@ -17,4 +17,14 @@
 #
 ###
 
-java -Dlog4j.configuration=file:./conf/log4j.xml -jar ./lib/opsGenie.jar "$@"
+# Uncomment to allow monitoring of this extension (define the POJO on com.appdynamics.extension.AlertingExtension.main())
+#MONITOR_WITH_AGENT=true
+
+
+if [ "x$MONITOR_WITH_AGENT" = "x" ]; then
+        echo "Extension monitoring is disabled"
+    else
+        JAVA_OPT="-javaagent:../../../appserver/glassfish/domains/domain1/appagent/javaagent.jar -Dappdynamics.cron.vm=true -Dappdynamics.controller.hostName=localhost -Dappdynamics.controller.port=8090 -Dappdynamics.agent.accountAccessKey=SJ5b2m7d1\$354 -Dappdynamics.agent.accountName=customer1 -Dappdynamics.agent.applicationName=OpsGenieExt -Dappdynamics.agent.tierName=OpsGenie -Dappdynamics.agent.nodeName=OpsGenie"
+    fi
+
+java  $JAVA_OPT -Dlog4j.configuration=file:./conf/log4j.xml -jar ./lib/opsGenie.jar "$@"
